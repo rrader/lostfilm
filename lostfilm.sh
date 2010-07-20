bb1b143460313673d56f220629b225e6a8244142
Antigluk <antigluk@gmail.com>
2010-07-20 13:41:24 +0300

    config.lib - библиотека функций для работы с конфиг-файлом перемещение функций конфига из lostfilm.sh в config.lib

--- lostfilm.sh
+++ lostfilm.sh
@@ -30,20 +30,6 @@ FORCE=0
 BASEDIRECTORY="`dirname $0`"
 #конец инициализации
 
-read_config(){
-	IFS=$'\n'
-	index=0
-
-	while read line ; do
-		name_lines[$index]="`echo "$line" | awk '-F|' '{ print $1 }'`"
-		gname_lines[$index]="`echo "$line" | awk '-F|' '{ print $2 }'`"
-		url_lines[$index]="`echo "$line" | awk '-F|' '{ print $3 }'`"
-		path_lines[$index]="`echo "$line" | awk '-F|' '{ print $4 }' | sed "s/%GNAME%/${gname_lines[$index]}/g"`"
-		index=$(($index+1))
-	done < $CONFIG_FILE
-	return 0
-}
-
 read_params(){
 	while [ -n "$1" ]; do
 		case $1 in
@@ -73,26 +59,6 @@ read_params(){
 	done
 }
 
-echo_serial_info(){
-	echo -e "${C_cyan}Название:    ${C_YELLOW}$1${C_NONE}"
-	echo -e "${C_cyan}Кодовое имя: ${C_YELLOW}$2${C_NONE}"
-	echo -e "${C_cyan}URL:         ${C_YELLOW}$3${C_NONE}"
-	echo -e "${C_cyan}Папка:       ${C_YELLOW}$4${C_NONE}"
-}
-
-echo_config_info(){
-	i=0
-	if [ $SERNUM -eq -1 ]; then
-		for gname in ${gname_lines[@]}; do
-			echo_serial_info ${gname} ${name_lines[$i]} ${url_lines[$i]} ${path_lines[$i]}
-			echo -e "${C_CYAN}---------------------------------------------${C_NONE}"
-			i=$(($i+1))
-		done
-	else
-		echo_serial_info ${gname_lines[$SERNUM]} ${name_lines[$SERNUM]} ${url_lines[$SERNUM]} ${path_lines[$SERNUM]}
-	fi
-}
-
 # библиотека для работы с лостфильмом
 . $BASEDIRECTORY/lostfilm.lib
 # библиотека для работы с базой данных
@@ -103,6 +69,8 @@ echo_config_info(){
 . $BASEDIRECTORY/colors.lib
 # библиотека для работы с торрент-клиентом
 . $BASEDIRECTORY/btclient.lib
+# библиотека для работы с конфиг-файлом
+. $BASEDIRECTORY/config.lib
 
 read_params "$@"
 read_config
